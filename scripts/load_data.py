f4e5c9d6dfe93bda4224e79d671117f1c500c0f2
AschalewMathewosDamtew <aschalewthecoder@gmail.com>
2024-09-18 23:39:24 +0300

    Add PostgreSQL data loading script and Jupyter notebook for querying data
    - Implement load_data.py script to load data from PostgreSQL using environment variables
    - Update .gitignore to exclude .env and venv directories

---
+++ scripts/load_data.py
@@ -0,0 +1,43 @@
+# scripts/load_data.py
+
+import os
+import psycopg2
+import pandas as pd
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+#Fetch database connection parameters from environment variables
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER ")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+def load_data_from_postgres(query):
+    """
+    Connects to the PostgreSQL database and loads data based on the provided SQL query.
+    
+    :param query: SQL query to execute.
+    :return: DataFrame containing the results of the query.
+    """
+    try:
+        # Establish a connection to the database
+        connection = psycopg2.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD
+        )
+        # Load data using pandas
+        df = pd.read_sql_query(query, connection)
+
+        # Close the database connection
+        connection.close()
+
+        return df
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
